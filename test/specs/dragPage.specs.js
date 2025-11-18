625da93e82c41035d3b60b28adc651dff149d74e
Geradot <toshkageradot@tuta.io>
2025-11-18 12:29:20 +0100

    feat: add DragPage tests for drag and drop functionality

---
+++ test/specs/dragPage.specs.js
@@ -0,0 +1,68 @@
+import dragPage from "../pageobjects/DragPage.js";
+import { restartAppAfterEachTest } from "../helpers/utils.js";
+
+describe("Drag Page", () => {
+  const elements = ["l1", "l2", "l3", "c1", "c2", "c3", "r1", "r2", "r3"];
+  let element;
+
+  beforeEach(async () => {
+    await dragPage.open();
+  });
+
+  afterEach(async function () {
+    await restartAppAfterEachTest(this);
+  });
+
+  it(`should successfully drag an element to correct place`, async () => {
+    element = elements[Math.floor(Math.random() * elements.length)];
+
+    await dragPage.dragAndDropByElement(element);
+
+    const isInDropZone = await dragPage.isInCorrectPosition(element);
+    await expect(isInDropZone).toBe(true);
+  });
+
+  it("should verify failed drag keeps an element at source", async () => {
+    let dragElement = "",
+      targetPlace = "";
+    // Take two different elements to simulate failed drag
+    while (dragElement === targetPlace) {
+      dragElement = elements[Math.floor(Math.random() * elements.length)];
+      targetPlace = elements[Math.floor(Math.random() * elements.length)];
+    }
+
+    await dragPage.dragAndDropByElement(dragElement, targetPlace);
+
+    const stillAtSource = await dragPage.isDragElementPresent(dragElement);
+    await expect(stillAtSource).toBe(true);
+  });
+
+  it("should successfully drag all elements into correct places", async () => {
+    await dragPage.dragMultiple([...elements].sort(() => Math.random() - 0.5));
+
+    await expect(dragPage.congratsMsg).toBeDisplayed();
+    await dragPage.clickRetryButton();
+
+    await expect(await dragPage.getAllDragElements()).toHaveLength(
+      elements.length
+    );
+  });
+
+  it("should successfully reset after dragging an element", async () => {
+    element = elements[Math.floor(Math.random() * elements.length)];
+
+    await expect(await dragPage.getAllDragElements()).toHaveLength(
+      elements.length
+    );
+
+    await dragPage.dragAndDropByElement(element);
+    await expect(await dragPage.getAllDragElements()).toHaveLength(
+      elements.length - 1
+    );
+
+    await dragPage.clickResetButton();
+    await expect(await dragPage.getAllDragElements()).toHaveLength(
+      elements.length
+    );
+  });
+});
